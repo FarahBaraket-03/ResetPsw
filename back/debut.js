3fe64061545d470a01c85600c47a8f530da0b5cb
FarahBaraket-03 <fbaraket48@gmail.com>
2024-12-04 19:03:43 +0100

    Fix bcrypt issue and switch to bcryptjs

--- back/debut.js
+++ back/debut.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const cors= require('cors');
 const mysql=require("mysql");
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const app= express();
 const dotenv=require('dotenv');
 
